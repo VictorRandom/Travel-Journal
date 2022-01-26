c5c31cdf2f937bd84a3620e43c984c041693ee0b
Victor Miranda Balduino <victormbalduino@hotmail.com>
2022-01-26 10:57:33 -0300

    criando card modelo e salvando os dados no data

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,22 +5,22 @@ export default function Card(){
     
     return(
         <div className="card">
-            <img src="./mount-fuji.jpg" alt="mount fuji" style={{width: '125px', height: '168px'}}></img>
-            <div>
-                <div id="">
+            <img src="./mount-fuji.jpg" alt="mount fuji" id="card-picture"></img>
+            <div id="card-info">
+                <div id="country">
                     <img src="./maps.png" alt="maps" style={{width: '10px',}}></img>
                     <div id="location">JAPAN</div>
-                    <div id="google-maps">View on Google Maps</div>
+                    <a href="https://www.google.com/maps/place/Jap%C3%A3o/" id="google-maps">View on Google Maps</a>
                 </div>
-                <div>
-                    <h2>Mount Fuji</h2>
+                <div id="place">
+                    <div>Mount Fuji</div>
                 </div>
-                <div>
+                <div id="date">
                     12 Jan, 2021 - 24 Jan, 2021
                 </div>
-                <p>
+                <div id="description">
                     Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-                </p>
+                </div>
             </div>
         </div>
     )
---
+++ src/components/data.js
@@ -0,0 +1,59 @@
+export default [
+  {
+    id: 1,
+    img: "mount-fuji.jpg",
+    location: "JAPAN",
+    maps: "Monte+Fuji",
+    place: " Mount Fuji",
+    initialDate: {
+      day: 12,
+      month: "Jan",
+      year: 2021,
+    },
+    endDate: {
+      day: 24,
+      month: "Jan",
+      year: 2021,
+    },
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    id: 2,
+    img: "sydney.jpg",
+    location: "AUSTRALIA",
+    maps: "Ópera+de+Sydney",
+    place: "Sydney Opera House",
+    initialDate: {
+      day: 27,
+      month: "May",
+      year: 2021,
+    },
+    endDate: {
+      day: 08,
+      month: "Jun",
+      year: 2021,
+    },
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+  },
+  {
+    id: 3,
+    img: "norway.jpg",
+    location: "NORWAY",
+    maps: "Fiorde+de+Geiranger",
+    place: "Geirangerfjord",
+    initialDate: {
+      day: 01,
+      month: "Oct",
+      year: 2021,
+    },
+    endDate: {
+      day: 18,
+      month: "Nov",
+      year: 2021,
+    },
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+  }
+];
