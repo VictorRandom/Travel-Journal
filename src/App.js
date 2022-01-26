def548e84d2635abdc05a0a00bd428160ba1dbfc
Victor Miranda Balduino <victormbalduino@hotmail.com>
2022-01-26 11:52:47 -0300

    automatizando dados do card

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import React from 'react';
 import './App.css'
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import data from './components/data';
 
 function App() {
+
+  console.log(data)
+  
+  const cardInfo = data.map((card) => {
+    return (
+    <Card 
+      id = {card.id}
+      img = {card.img}
+      location = {card.location}
+      maps = {card.maps}
+      place = {card.place}
+      dayInitalDate = {card.initialDate.day}
+      monthInitalDate = {card.initialDate.month}
+      yearInitalDate = {card.initialDate.year}
+      dayEndDate = {card.endDate.day}
+      monthEndDate = {card.endDate.month}
+      yearEndDate = {card.endDate.year}
+      description = {card.description}
+    />
+    )
+  });
+
   return (
     <div className="App">
       <Navbar />
       <div className="cards">
-        <Card />
-        <Card />
-        <Card />
+        {cardInfo}
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React from "react";
 
-
-export default function Card(){
-    
-    return(
-        <div className="card">
-            <img src="./mount-fuji.jpg" alt="mount fuji" id="card-picture"></img>
-            <div id="card-info">
-                <div id="country">
-                    <img src="./maps.png" alt="maps" style={{width: '10px',}}></img>
-                    <div id="location">JAPAN</div>
-                    <a href="https://www.google.com/maps/place/Jap%C3%A3o/" id="google-maps">View on Google Maps</a>
-                </div>
-                <div id="place">
-                    <div>Mount Fuji</div>
-                </div>
-                <div id="date">
-                    12 Jan, 2021 - 24 Jan, 2021
-                </div>
-                <div id="description">
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-                </div>
-            </div>
+export default function Card(props) {
+    console.log(props)
+  return (
+    <div className="card">
+      <img src={`./${props.img}`} alt="mount fuji" id="card-picture"></img>
+      <div id="card-info">
+        <div id="country">
+          <img src="./maps.png" alt="maps" style={{ width: "10px" }}></img>
+          <div id="location">{props.location}</div>
+          <a
+            href={`https://www.google.com/maps/place/${props.maps}/`}
+            id="google-maps"
+          >
+            View on Google Maps
+          </a>
         </div>
-    )
+        <div id="place">
+          <div>{props.place}</div>
+        </div>
+        <div id="date">{props.dayInitalDate} {props.monthInitalDate}, {props.yearInitalDate} - {props.dayEndDate} {props.monthEndDate}, {props.yearEndDate}</div>
+        <div id="description">
+            {props.description}
+        </div>
+      </div>
+    </div>
+  );
 }
